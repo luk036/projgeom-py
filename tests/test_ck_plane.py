4a5728e0994266e2d2a770458b4c4117ed62446d
Wai-Shing Luk <luk036@gmail.com>
2022-11-04 16:46:17 +0800

    add hypothesis tests

--- tests/test_ck_plane.py
+++ tests/test_ck_plane.py
@@ -1,4 +1,5 @@
-import pytest
+from hypothesis import given
+from hypothesis.strategies import integers
 
 # from projgeom_py.pg_plane import coincident, harm_conj
 from projgeom_py.ck_plane import (
@@ -26,50 +27,57 @@ def check_ck_plane(a1: CKPlane, a2: CKPlane, a3: CKPlane):
     assert o == t2.circ(t3)
 
 
-def test_ell_point(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_ell_point(a1z, a2z, a3y):
     a1 = EllPoint([13, 23, a1z])
     a2 = EllPoint([44, -3, a2z])
-    a3 = EllPoint([-2, 12, a3z])
+    a3 = EllPoint([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_ell_line(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_ell_line(a1z, a2z, a3y):
     a1 = EllLine([13, 23, a1z])
     a2 = EllLine([44, -3, a2z])
-    a3 = EllLine([-2, 12, a3z])
+    a3 = EllLine([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_hyp_point(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_hyp_point(a1z, a2z, a3y):
     a1 = HypPoint([13, 23, a1z])
     a2 = HypPoint([44, -3, a2z])
-    a3 = HypPoint([-2, 12, a3z])
+    a3 = HypPoint([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_hyp_line(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_hyp_line(a1z, a2z, a3y):
     a1 = HypLine([13, 23, a1z])
     a2 = HypLine([44, -3, a2z])
-    a3 = HypLine([-2, 12, a3z])
+    a3 = HypLine([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_myck_point(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_myck_point(a1z, a2z, a3y):
     a1 = MyCKPoint([13, 23, a1z])
     a2 = MyCKPoint([44, -3, a2z])
-    a3 = MyCKPoint([-2, 12, a3z])
+    a3 = MyCKPoint([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_myck_line(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_myck_line(a1z, a2z, a3y):
     a1 = MyCKLine([13, 23, a1z])
     a2 = MyCKLine([44, -3, a2z])
-    a3 = MyCKLine([-2, 12, a3z])
+    a3 = MyCKLine([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
 
 
-def test_persp_point(a1z, a2z, a3z):
+@given(integers(), integers(), integers())
+def test_persp_point(a1z, a2z, a3y):
     a1 = PerspPoint([13, 23, a1z])
     a2 = PerspPoint([44, -3, a2z])
-    a3 = PerspPoint([-2, 12, a3z])
+    a3 = PerspPoint([-2, a3y, 12])
     check_ck_plane(a1, a2, a3)
--- tests/test_proj_plane.py
+++ tests/test_proj_plane.py
@@ -1,4 +1,5 @@
-import pytest
+from hypothesis import given
+from hypothesis.strategies import integers
 
 from projgeom_py.pg_plane import ProjPlane, coincident, harm_conj
 from projgeom_py.pg_point import PgLine, PgPoint
@@ -16,12 +17,14 @@ def check_pg_plane(p: ProjPlane, q: ProjPlane):
     assert harm_conj(p, q, h) == pq
 
 
+@given(integers(), integers())
 def test_pg_point(pz, qz):
     p = PgPoint([1, 3, pz])
     q = PgPoint([-2, 1, qz])
     check_pg_plane(p, q)
 
 
+@given(integers(), integers())
 def test_pg_line(pz, qz):
     p = PgLine([1, 3, pz])
     q = PgLine([-2, 1, qz])
