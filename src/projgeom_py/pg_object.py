f0ca4416c1857f901eebbfba0d06f20d0aaebba0
Wai-Shing Luk <luk036@gmail.com>
2023-07-30 15:13:37 +0000

    add docment and examples

--- src/projgeom_py/pg_object.py
+++ src/projgeom_py/pg_object.py
@@ -8,37 +8,39 @@ Dual = TypeVar("Dual", bound="PgObject")
 
 
 def dot(a: List[int], b: List[int]) -> int:
-    """_summary_
-
-    Args:
-        a (List[int]): _description_
-        b (List[int]): _description_
-
-    Returns:
-        int: _description_
+    """
+    The `dot` function calculates the dot product of two lists of integers.
+    
+    :param a: a is a list of integers
+    :type a: List[int]
+    :param b: The parameter `b` is a list of integers
+    :type b: List[int]
+    :return: The function `dot` returns the dot product of two lists of integers.
 
     Examples:
         >>> dot([1, 2, 3], [4, 5, 6])
         32
-
+        >>> dot([1, 2, 3], [4, 5, 6]) == 32
+        True
     """
     return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
 
 
 def cross(a: List[int], b: List[int]) -> List[int]:
-    """_summary_
-
-    Args:
-        a (List[int]): _description_
-        b (List[int]): _description_
-
-    Returns:
-        List[int]: _description_
+    """
+    The `cross` function calculates the cross product of two vectors.
+    
+    :param a: a is a list of integers
+    :type a: List[int]
+    :param b: The parameter `b` is a list of integers
+    :type b: List[int]
+    :return: The function `cross` returns a list of three integers.
 
     Examples:
         >>> cross([1, 2, 3], [4, 5, 6])
         [-3, 6, -3]
-
+        >>> cross([1, 2, 3], [4, 5, 6]) == [-3, 6, -3]
+        True
     """
     return [
         a[1] * b[2] - a[2] * b[1],
@@ -48,21 +50,27 @@ def cross(a: List[int], b: List[int]) -> List[int]:
 
 
 def plckr(ld: int, p: List[int], mu: int, q: List[int]) -> List[int]:
-    """_summary_
-
-    Args:
-        ld (int): _description_
-        p (List[int]): _description_
-        mu (int): _description_
-        q (List[int]): _description_
-
-    Returns:
-        List[int]: _description_
+    """
+    The `plckr` function calculates the Plucker operation by multiplying each element of `p` by `ld` and
+    each element of `q` by `mu`, and then adding the corresponding elements together.
+    
+    :param ld: ld is an integer representing the scalar coefficient for the first vector p in the
+    Plucker operation
+    :type ld: int
+    :param p: The parameter `p` is a list of three integers
+    :type p: List[int]
+    :param mu: The `mu` parameter represents a scalar value that is used in the Plucker operation. It is
+    multiplied with the corresponding elements of the `q` list to calculate the result
+    :type mu: int
+    :param q: The parameter `q` is a list of integers
+    :type q: List[int]
+    :return: The `plckr` function returns a list of three integers.
 
     Examples:
         >>> plckr(1, [1, 2, 3], 2, [4, 5, 6])
         [9, 12, 15]
-
+        >>> plckr(1, [1, 2, 3], 2, [4, 5, 6]) == [9, 12, 15]
+        True
     """
     return [
         ld * p[0] + mu * q[0],
@@ -71,35 +79,49 @@ def plckr(ld: int, p: List[int], mu: int, q: List[int]) -> List[int]:
     ]
 
 
+# The `PgObject` class represents a geometric object in a projective plane with integer coordinates.
 class PgObject(ProjPlane[Dual, int]):
+    """
+    The `PgObject` class represents a geometric object in a projective plane with integer coordinates.
+
+    :param coord: The `coord` parameter represents a list of three integers that represent the
+    coordinates of the geometric object.
+    :type coord: List[int]
+    :raises ValueError: The `coord` parameter must be a list of three integers.
+
+    Examples:
+        >>> p = PgObject([3, 4, 5])
+        >>> p.coord
+        [3, 4, 5]
+    """
     coord: List[int]
 
     # impl PgObject:
 
     def __init__(self, coord: List[int]) -> None:
-        """_summary_
-
-        Args:
-            coord (List[int]): _description_
+        """
+        The function initializes an object with a given coordinate.
+        
+        :param coord: The `coord` parameter is a list of integers that represents the coordinates of a
+        point in a three-dimensional space
+        :type coord: List[int]
 
         Examples:
            >>> p = PgObject([3, 4, 5])
            >>> p.coord
            [3, 4, 5]
-
         """
         self.coord = coord
 
     # impl PartialEq for PgObject:
 
     def __eq__(self, other) -> bool:
-        """_summary_
-
-        Args:
-            other (PgObject): _description_
-
-        Returns:
-            bool: _description_
+        """
+        The function checks if two PgObject instances are equal by comparing their coordinates.
+        
+        :param other: The `other` parameter is of type `PgObject`
+        :return: The `__eq__` method is returning a boolean value. It returns `True` if the `coord`
+        attribute of `self` and `other` are equal, and `False` otherwise.
 
         Examples:
            >>> p = PgObject([3, 4, 5])
@@ -118,10 +140,9 @@ class PgObject(ProjPlane[Dual, int]):
         pass
 
     def aux(self) -> Dual:
-        """_summary_
-
-        Returns:
-            PgLine: _description_
+        """
+        The `aux` function returns a `Dual` object with a copy of the `coord` attribute.
+        :return: The `aux` function is returning a `Dual` object.
         """
         # L = self.dual()
         return self.dual()(self.coord.copy())
@@ -140,7 +161,6 @@ class PgObject(ProjPlane[Dual, int]):
             >>> q = PgObject([30, 40, 50])
             >>> p.dot(q)
             500
-
         """
         return dot(self.coord, line.coord)
 
@@ -157,11 +177,10 @@ class PgObject(ProjPlane[Dual, int]):
             PgObject: _description_
 
         Examples:
-            >>> p = PgObject([3, 4, 5])
-            >>> q = PgObject([30, 40, 50])
+            >>> p = PgObject([1, 2, 3])
+            >>> q = PgObject([4, 5, 6])
             >>> p.plucker(1, q, 2) == PgObject([9, 12, 15])
             True
-
         """
         P = type(self)
         return P(plckr(ld, self.coord, mu, q.coord))
@@ -176,13 +195,6 @@ class PgObject(ProjPlane[Dual, int]):
 
         Returns:
             bool: _description_
-
-        Examples:
-            >>> p = PgObject([3, 4, 5])
-            >>> q = PgObject([30, 40, 50])
-            >>> p.incident(q)
-            False
-
         """
         return dot(self.coord, rhs.coord) == 0
 
--- src/projgeom_py/pg_point.py
+++ src/projgeom_py/pg_point.py
@@ -2,21 +2,40 @@ from .pg_object import PgObject
 
 
 class PgPoint(PgObject["PgLine"]):
-    """_summary_
+    """Projective Geometry Point
 
-    Args:
-        PgObject (_type_): _description_
+    The `PgPoint` class represents a point in projective geometry and has a method `dual()` that returns
+    the dual line of the point.
+
+    Examples:
+        >>> from projgeom_py.pg_point import PgPoint, PgLine
+        >>> p = PgPoint([1, 2, 3])
+        >>> l = p.aux()
+        >>> assert isinstance(l, PgLine)
+        >>> assert not p.incident(l)
     """
 
     def dual(self) -> type:
-        """_summary_
-
-        Returns:
-            type: _description_
+        """
+        The `dual` function returns the type `PgLine`.
+        :return: The `dual` method is returning the type `PgLine`.
         """
         return PgLine
 
 
 class PgLine(PgObject[PgPoint]):
+    """Projective Geometry Line
+
+    The `PgLine` class represents a projective geometry line and has a method `dual()` that returns the
+    dual object, which is a `PgPoint`.
+
+    Examples:
+        >>> from projgeom_py.pg_point import PgPoint, PgLine
+        >>> l = PgLine([1, 2, 3])
+        >>> p = l.aux()
+        >>> assert isinstance(p, PgPoint)
+        >>> assert not l.incident(p)
+    """
+
     def dual(self) -> type:
         return PgPoint
