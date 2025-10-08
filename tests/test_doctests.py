5c011b22df331bb369f9a4335777e82676c90086
Wai-Shing Luk <luk036@gmail.com>
2025-10-08 03:07:46 +0000

    apply pre-commit run --all-files

--- tests/test_doctests.py
+++ tests/test_doctests.py
@@ -1,5 +1,6 @@
 import doctest
 import projgeom.pg_plane
 
+
 def test_doctests():
     assert doctest.testmod(projgeom.pg_plane).failed == 0
--- tests/test_pg_object_doctests.py
+++ tests/test_pg_object_doctests.py
@@ -1,5 +1,6 @@
 import doctest
 import projgeom.pg_object
 
+
 def test_doctests():
     assert doctest.testmod(projgeom.pg_object).failed == 0
